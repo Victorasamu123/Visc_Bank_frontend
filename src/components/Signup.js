46b64b98e5b786d24ac5f076a191f9c22bc47e5d
Asamu victor <asamuvictor03@gmail.com>
2022-08-27 21:36:53 +0100

    added new features to home page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from "axios"
 import {Link} from "react-router-dom"
 import "../styles/Signup.css"
 const Signup = () => {
+  const [firstname, setfirstname] = useState("");
+  const [lastname, setlastname] = useState("");
+  const [email, setemail] = useState("");
+  const [phone, setphone] = useState("");
+  const [passsword, setpasssword] = useState("");
+  const [acctno, setacctno] = useState("");
+  const [initialbalance, setinitialbalance] = useState("");
     const signup=()=>{
         alert("e don dey sign o")
     }
@@ -12,11 +19,11 @@ const Signup = () => {
        <h3 className='pt-5'>Signup</h3>
        <center>
         <div className='div-input'>
-        <input type="text"  placeholder='Firstname' className='input-area'/>
-        <input type="text"  placeholder='Lasttname' className='input-area'/>
-        <input type="text"  placeholder='Phonenumber' className='input-area'/>
-        <input type="email"  placeholder='Email address' className='input-area'/>
-        <input type="password"  placeholder='Password' className='input-area'/>
+        <input type="text"  placeholder='Firstname' className='input-area' onChange={(e)=>setfirstname(e.target.value)}/>
+        <input type="text"  placeholder='Lasttname' className='input-area' onChange={(e)=>setlastname(e.target.value)}/>
+        <input type="number"  placeholder='Phonenumber' className='input-area' onChange={(e)=>setphone(e.target.value)}/>
+        <input type="email"  placeholder='Email address' className='input-area' onChange={(e)=>setemail(e.target)}/>
+        <input type="password"  placeholder='Password' className='input-area' onChange={(e)=>set}/>
         <button className='btn w-25 mt-3' onClick={signup}>signup</button>
         <p>
             Already have an account?<Link to="/signin" className='link'>sign in here</Link>

6fe7615e3dcdad0d4c1f3c16c22109f397f2332d
Asamu victor <asamuvictor03@gmail.com>
2022-08-27 11:56:24 +0100

    completed navbar and started home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,36 @@
 import React from 'react'
-
+import "../styles/Home.css"
+import img from "../images/image-removebg-preview.png"
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div className='div-text'>
+        <h1>
+          Make your <br />
+          transactions even <br />
+          easier with Visc
+        </h1>
+        <div className='mini-text'>
+          We're here to help you get the best out of your<br />
+          money,no strings attached. <br />
+          Welcome to your freedom
+        </div>
+        <button className='btn mt-3'>
+          Sign up
+        </button>
+        <button className='btn ms-3 mt-3'>
+          Sign in
+        </button>
+        <div>
+          <img src={img} alt="" width={750} className="img-div" />
+        </div>
+      </div>
+      <div className='sec'>
+        <h1>
+          Why choose Visc bank?
+        </h1>
+      </div>
+    </>
   )
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,40 +4,46 @@ import "../styles/NavBar.css"
 const NavBar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-light" id='nav'>
-  <div className="container-fluid text-light">
-    <a className="navbar-brand text-light" href="#">
-        <img src={logo} alt="" width={90}/>
-        <span className='span'>Visc</span>
-    </a>
-    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-light" href="#">Features</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-light" href="#">Pricing</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
+      <nav className="navbar navbar-expand-lg bg-light sticky-top" id='nav'>
+        <div className="container-fluid text-light">
+          <a className="navbar-brand text-light ms-5 " href="#">
+            <span className='span'>Visc</span>
           </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div> */}
-  </div>
-</nav>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item me-5">
+                <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item me-5">
+                <a className="nav-link text-light" href="#">Features</a>
+              </li>
+              <li className="nav-item me-5">
+                <a className="nav-link text-light" href="#">Contact us</a>
+              </li>
+              <li className="nav-item dropdown me-5">
+                <a className="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Product
+                </a>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <div className='me-3'>
+          <div className='me-5 pe-5'>
+          <div className='button me-5'>
+            Get Visc
+          </div>
+          </div>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }

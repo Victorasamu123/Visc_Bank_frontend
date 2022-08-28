fac33376a672424afb647863fad9a3b27cbb53eb
Asamu victor <asamuvictor03@gmail.com>
2022-08-28 01:11:01 +0100

    added new new features to dashboard page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -5,13 +5,27 @@ import "../styles/Signup.css"
 const Signup = () => {
   const [firstname, setfirstname] = useState("");
   const [lastname, setlastname] = useState("");
-  const [email, setemail] = useState("");
   const [phone, setphone] = useState("");
+  const [email, setemail] = useState("");
   const [passsword, setpasssword] = useState("");
   const [acctno, setacctno] = useState("");
   const [initialbalance, setinitialbalance] = useState("");
-    const signup=()=>{
-        alert("e don dey sign o")
+  const endpoint="http://localhost:3500/users/signup"
+  // const endpoints="http://localhost:3500/users/test"
+  const signup=()=>{
+    // axios.get(endpoints).then((result)=>{
+    //   console.log(result)
+    // })
+    setacctno(Math.floor(Math.random()*100000000000))
+      setinitialbalance(5000);
+      let usersDetails={firstname,lastname,phone,email,passsword,acctno,initialbalance};
+      axios.post(endpoint,usersDetails).then((result)=>{
+        // console.log(result.data.message);
+        console.log(usersDetails)
+    }).catch((err)=>{
+      console.log(err.message);
+      console.log(err)
+    })
     }
   return (
     <>
@@ -20,10 +34,10 @@ const Signup = () => {
        <center>
         <div className='div-input'>
         <input type="text"  placeholder='Firstname' className='input-area' onChange={(e)=>setfirstname(e.target.value)}/>
-        <input type="text"  placeholder='Lasttname' className='input-area' onChange={(e)=>setlastname(e.target.value)}/>
+        <input type="text"  placeholder='Lastname' className='input-area' onChange={(e)=>setlastname(e.target.value)}/>
         <input type="number"  placeholder='Phonenumber' className='input-area' onChange={(e)=>setphone(e.target.value)}/>
         <input type="email"  placeholder='Email address' className='input-area' onChange={(e)=>setemail(e.target)}/>
-        <input type="password"  placeholder='Password' className='input-area' onChange={(e)=>set}/>
+        <input type="password"  placeholder='Password' className='input-area' onChange={(e)=>setpasssword(e.target.value)}/>
         <button className='btn w-25 mt-3' onClick={signup}>signup</button>
         <p>
             Already have an account?<Link to="/signin" className='link'>sign in here</Link>

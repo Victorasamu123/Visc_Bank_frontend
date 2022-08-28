28b91b826756e8178890a257c5b45473979c47b0
Asamu victor <asamuvictor03@gmail.com>
2022-08-28 15:14:46 +0100

    started functionalities

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from '../images/images-removebg-preview.png'
 import "../styles/NavBar.css"
 import {Link} from "react-router-dom"
+const endpoint="http://localhost:3500/users/signup"
 const NavBar = () => {
   return (
     <>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -5,31 +5,20 @@ import "../styles/Signup.css"
 const Signup = () => {
   const [firstname, setfirstname] = useState("");
   const [lastname, setlastname] = useState("");
-  const [Phonenumber, setPhonenumber] = useState(0);
+  const [Phonenumber, setPhonenumber] = useState("");
   const [email, setemail] = useState("");
-  const [passsword, setpasssword] = useState("");
-  const [acctno, setacctno] = useState(0);
-  const [initialbalance, setinitialbalance] = useState(0);
-  const [usersDetails, setusersDetails] = useState({});
-  const endpoint="http://localhost:3500/users/signup"
-  // const endpoints="http://localhost:3500/users/test"
+  const [password, setpassword] = useState("");
+  const [accountnumber, setaccountnumber] = useState(Math.floor(Math.random()*100000000000));
+  const [initialbalance, setinitialbalance] = useState("15000");
+
   const signup=()=>{
-    // axios.get(endpoints).then((result)=>{
-    //   console.log(result)
-    // })
-    setacctno(Math.floor(Math.random()*100000000000))
-      setinitialbalance(5000);
-      let newNigga={firstname,lastname,Phonenumber,email,passsword,acctno,initialbalance}
-      setusersDetails(newNigga)
-      axios.post(endpoint,usersDetails).then((result)=>{
-        // console.log(result.data.message);
-        console.log(usersDetails)
-    }).catch((err)=>{
-      console.log(err.message);
-      console.log(err)
-    })
-    }
-  return (
+    setaccountnumber(Math.floor(2+Math.random()*10000000000))
+    setinitialbalance(15000);
+    let userDetails={firstname,lastname,Phonenumber,email,password,accountnumber,initialbalance}
+    console.log(userDetails)
+    alert("hello express");
+  }
+return(
     <>
       <section className='bg-light'>
        <h3 className='pt-5'>Signup</h3>
@@ -38,8 +27,8 @@ const Signup = () => {
         <input type="text"  placeholder='Firstname' className='input-area' onChange={(e)=>setfirstname(e.target.value)}/>
         <input type="text"  placeholder='Lastname' className='input-area' onChange={(e)=>setlastname(e.target.value)}/>
         <input type="number"  placeholder='Phonenumber' className='input-area' onChange={(e)=>setPhonenumber(e.target.value)}/>
-        <input type="email"  placeholder='Email address' className='input-area' onChange={(e)=>setemail(e.target)}/>
-        <input type="password"  placeholder='Password' className='input-area' onChange={(e)=>setpasssword(e.target.value)}/>
+        <input type="email"  placeholder='Email address' className='input-area' onChange={(e)=>setemail(e.target.value)}/>
+        <input type="password"  placeholder='Password' className='input-area' onChange={(e)=>setpassword(e.target.value)}/>
         <button className='btn w-25 mt-3' onClick={signup}>signup</button>
         <p>
             Already have an account?<Link to="/signin" className='link'>sign in here</Link>

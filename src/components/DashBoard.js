021f0d95f1942b4c4723436522a2d4d0d93fa4c3
Asamu victor <asamuvictor03@gmail.com>
2022-08-30 21:35:13 +0100

    added new design to dashboard

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import axios from "axios"
 import {Link} from "react-router-dom"
 import "../styles/DashBoard.css"
@@ -9,6 +9,7 @@ import icon4 from "../images/wallet_FILL0_wght200_GRAD0_opsz48.svg"
 import icon5 from "../images/payments_FILL0_wght200_GRAD0_opsz48.svg"
 import icon6 from "../images/account_balance_wallet_FILL0_wght200_GRAD0_opsz48.svg"
 import icon7 from "../images/search-outline.svg"
+import icon8 from "../images/images-removebg-preview.png"
 const DashBoard = () => {
   return (
     <>
@@ -29,8 +30,14 @@ const DashBoard = () => {
            <div className='username-div'>Asamu victor</div>
            <div className='current-balance-div'>
              <h6 className='current-balance-text pt-3 ms-3'>Current Balance</h6>
-             <div></div>
-             <div className='amount-text'>$11000000</div>
+             <div className='master-card'><img src={icon8} alt="" width={35}/></div>
+             <div className='amount-text pt-2 ms-3'>$11000000</div>
+             <div className='account-number mt-5 pt-3 ms-3'>2494 9595 9595 9555</div>
+           </div>
+           <div className='basic-transactions'>
+              <div className=''>
+
+              </div>
            </div>
          </div>
        </div>
